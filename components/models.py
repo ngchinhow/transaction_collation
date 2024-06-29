8e9958907ea287c800b698e391c282575211b7fe
AmideWing <ngchinhow@gmail.com>
2024-06-29 23:20:48 +0800

    Able to parse UOB account statement metadata.

--- components/models.py
+++ components/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from polymorphic.models import PolymorphicModel
+from dataclasses import dataclass
 
 
+@dataclass(init=False)
 # Create your models here.
 class Address(models.Model):
     full_address = models.CharField(max_length=255)
 
 
+@dataclass(init=False)
 class FinancialInstitution(models.Model):
     full_name = models.CharField(max_length=255)
     abbreviation = models.CharField(max_length=5)
@@ -16,11 +19,13 @@ class FinancialInstitution(models.Model):
     email = models.EmailField(max_length=255)
 
 
+@dataclass(init=False)
 class InstrumentHolder(models.Model):
     full_name = models.CharField(max_length=255)
     address = models.ForeignKey(Address, on_delete=models.DO_NOTHING)
 
 
+@dataclass(init=False)
 class Instrument(PolymorphicModel):
     name = models.CharField(max_length=255)
     holder = models.ForeignKey(InstrumentHolder, on_delete=models.DO_NOTHING)
@@ -29,16 +34,19 @@ class Instrument(PolymorphicModel):
     currency = models.CharField(max_length=3)
 
 
+@dataclass(init=False)
 class Account(Instrument):
     type = models.CharField(max_length=10)
     credit_line = models.DecimalField(max_digits=20, decimal_places=2)
     balance = models.DecimalField(max_digits=20, decimal_places=2)
 
 
+@dataclass(init=False)
 class Card(Instrument):
     total_credit_limit = models.PositiveIntegerField('total credit limit')
 
 
+@dataclass(init=False)
 class Transaction(PolymorphicModel):
     instrument = models.ForeignKey(Instrument, on_delete=models.DO_NOTHING)
     date = models.DateField('transaction date', null=True)
@@ -47,12 +55,14 @@ class Transaction(PolymorphicModel):
     amount = models.DecimalField(max_digits=20, decimal_places=2)
 
 
+@dataclass(init=False)
 class AccountTransaction(Transaction):
     # withdrawals are considered transaction amounts
     deposits = models.DecimalField(max_digits=20, decimal_places=2)
     balance = models.DecimalField(max_digits=20, decimal_places=2)
 
 
+@dataclass(init=False)
 class CardTransaction(Transaction):
     # transaction date is the date used for base transactions
     post_date = models.DateField('post date', null=True)
--- document_consumer/uob/uob_parsers.py
+++ document_consumer/uob/uob_parsers.py
@@ -1,8 +1,12 @@
 from datetime import datetime
-from components.models import FinancialInstitution, Address, InstrumentHolder
+from typing import List, cast
+
+from pdf_reader.custom_dataclasses import ExtractedPage, ExtractedTable, PdfParagraph
+
+from components.models import FinancialInstitution, Address, InstrumentHolder, Account
 
 
-def parse_uob_statement(pages, fi_information):
+def parse_uob_statement(pages: List[ExtractedPage], fi_information):
     fi_address = Address(full_address=fi_information[1])
     fi = FinancialInstitution(full_name=fi_information[0],
                               abbreviation='UOB',
@@ -11,25 +15,74 @@ def parse_uob_statement(pages, fi_information):
                               gst_registration_number=fi_information[3].replace('GST Reg. No. ', ''),
                               email=fi_information[4])
 
-    print(pages[0].paragraphs[2])
-    first_page_second_paragraph_elements = pages[0].paragraphs[2].elements
-    print(first_page_second_paragraph_elements)
+    first_page_second_paragraph_elements = cast(PdfParagraph, pages[0].paragraphs[2]).elements
     if first_page_second_paragraph_elements[0].get_text() == 'Statement of Account':
         parse_uob_account_statement(pages, fi, first_page_second_paragraph_elements[1].get_text())
 
 
-def parse_uob_account_statement(pages, fi: FinancialInstitution, period: str):
-    first_page_first_element_words = pages[0].elements[0].get_text().split(' ')
+def parse_uob_account_statement(pages: List[ExtractedPage], fi: FinancialInstitution, period: str):
+    accounts, statement_year = parse_uob_account_metadata(pages[0], fi, period)
+    print(accounts)
+    print(statement_year)
+
+
+def parse_uob_account_metadata(first_page: ExtractedPage, fi: FinancialInstitution, period: str):
+    # Instrument holder name
+    first_page_first_element_words = first_page.elements[0].get_text().split(' ')
     instrument_holder_name = ' '.join([word.capitalize() for word in first_page_first_element_words[1:]])
 
-    first_page_third_element = pages[0].elements[2].get_text()
-    for item in pages[0].elements[3].items:
+    # Instrument holder address
+    first_page_third_element = first_page.elements[2].get_text()
+    for item in cast(ExtractedTable, first_page.elements[3]).items:
         first_page_third_element += ' ' + item.el.text.replace(' Call', '')
     instrument_holder_address_text = ' '.join([word.capitalize() for word in first_page_third_element.split(' ')])
     instrument_holder_address = Address(full_address=instrument_holder_address_text)
     instrument_holder = InstrumentHolder(full_name=instrument_holder_name, address=instrument_holder_address)
 
+    # Accounts
+    first_page_seventh_paragraph_items = cast(ExtractedTable, first_page.paragraphs[6]).items
+    currency_x_begin_coor = None
+    credit_line_x_end_coor = None
+    for group in first_page_seventh_paragraph_items[0].base_element_groups:
+        if group.text == 'Currency':
+            currency_x_begin_coor = group.x0
+        elif group.text == 'Credit Line':
+            credit_line_x_end_coor = group.x1
+
+    accounts = {}
+    for account_table_line in first_page_seventh_paragraph_items[1:]:
+        line_y_coor = account_table_line.el.y0
+        balance = account_table_line.values[0]
+        currency = None
+        credit_line = None
+        for group in account_table_line.base_element_groups:
+            if group.x0 == currency_x_begin_coor:
+                currency = group.text
+            elif group.x1 == credit_line_x_end_coor:
+                from decimal import Decimal
+                credit_line = Decimal(group.text)
+
+        if currency is not None:
+            accounts[line_y_coor] = Account(holder=instrument_holder,
+                                            provider=fi,
+                                            currency=currency,
+                                            credit_line=credit_line,
+                                            balance=balance)
+
+    for account_detail_paragraph in first_page.paragraphs[7:7 + len(accounts)]:
+        account_detail_paragraph = cast(PdfParagraph, account_detail_paragraph)
+        account = accounts.pop(account_detail_paragraph.elements[1].y0)
+        account_type, account_name, account_number = (account_detail_paragraph.text
+                                                      .split(account_detail_paragraph.line_break_char))
+        account.type = account_type
+        account.name = account_name
+        account.number = account_number
+
+        accounts[account_number] = account
+
+    # Statement year
     import re
     period_search = re.search('Period: .+ to (\\d{2} \\w{3} \\d{4})', period)
-    period_end = datetime.strptime(period_search.group(1), '%d %b %Y').date()
-    print(period_end)
+    statement_year = datetime.strptime(period_search.group(1), '%d %b %Y').date().year
+
+    return accounts, statement_year
