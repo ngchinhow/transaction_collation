5f0717743cbeac626fa5e189eace245ba8e3a997
AmideWing <ngchinhow@gmail.com>
2024-07-20 16:24:17 +0800

    Renamed database

--- transaction_collation/settings.py
+++ transaction_collation/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'transaction_collation.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'transaction_collation.sqlite3',
     }
 }
 
