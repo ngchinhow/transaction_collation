851b2992dc3b86e0cce968fbd13a872d5d87e3f9
AmideWing <ngchinhow@gmail.com>
2024-07-29 00:57:09 +0800

    Added POSB account parser.

---
+++ document_consumer/posb/account_parser.py
@@ -0,0 +1,68 @@
+import re
+from datetime import datetime
+from decimal import Decimal
+
+from django.contrib.contenttypes.models import ContentType
+
+from components.models import FinancialInstitution, \
+    InstrumentHolder, \
+    Account, \
+    Statement, \
+    InstrumentStatement, \
+    AccountSnapshot, AccountTransaction
+
+
+def parse_posb_account_transactions(file_name: str, holder: InstrumentHolder, currency: str, rows: list):
+    # Financial institution and account
+    account_details = re.search('^(\\w+) ([\\w\\s]+?) (\\w+) Account ([\\d-]+)$', rows[0][1])
+    fi, fi_created = FinancialInstitution.objects.get_or_create(abbreviation=account_details.group(1))
+    account_content_type = ContentType.objects.get_for_model(Account)
+    account, account_created = Account.objects.get_or_create(holder=holder,
+                                                             provider=fi,
+                                                             name=account_details.group(2),
+                                                             number=account_details.group(4),
+                                                             currency=currency,
+                                                             type=account_details.group(3))
+
+    # Statement
+    statement_date = datetime.strptime(rows[1][1].strip(), '%d %b %Y').date()
+    statement, statement_created = Statement.objects.get_or_create(holder=holder,
+                                                                   provider=fi,
+                                                                   file_name=file_name,
+                                                                   date=statement_date,
+                                                                   type=Statement.InstrumentType.ACCOUNT)
+
+    # Instrument statement and account snapshot
+    balance = Decimal(rows[2][1])
+    account_statement, account_statement_created = (InstrumentStatement.objects
+                                                    .get_or_create(instrument_content_type=account_content_type,
+                                                                   instrument_id=account.id,
+                                                                   statement=statement))
+    account_snapshot, account_snapshot_created = (AccountSnapshot.objects
+                                                  .get_or_create(instrument_statement=account_statement,
+                                                                 defaults={
+                                                                     'credit_line': Decimal(0),
+                                                                     'balance': balance
+                                                                 }))
+
+    # Transactions
+    account_snapshot_content_type = ContentType.objects.get_for_model(AccountSnapshot)
+
+    for i, row in enumerate(rows[5:]):
+        transaction_date = datetime.strptime(row[0], '%d %b %Y').date()
+        debit = Decimal(row[2]) if row[2].strip() != '' else None
+        credit = Decimal(row[3]) if row[3].strip() != '' else None
+        description = row[4].strip()
+        sub_description = '\n'.join([text.strip() for text in row[5:] if text.strip() != ''])
+
+        transaction, transaction_created = (AccountTransaction.objects
+                                            .get_or_create(snapshot_content_type=account_snapshot_content_type,
+                                                           snapshot_id=account_snapshot.id,
+                                                           row_number=i + 1,  # 1 begin list index
+                                                           defaults={
+                                                               'date': transaction_date,
+                                                               'description': description,
+                                                               'sub_description': sub_description,
+                                                               'amount': debit,
+                                                               'deposits': credit
+                                                           }))
--- document_consumer/services.py
+++ document_consumer/services.py
@@ -1,3 +1,4 @@
+import csv
 from pathlib import Path
 
 import django
@@ -6,15 +7,28 @@ from pdf_reader import get_elements_from_pdf
 django.setup()
 
 from document_consumer.uob.factory import parse_uob_statement
+from document_consumer.posb.account_parser import parse_posb_account_transactions
+from components.models import InstrumentHolder
 
 
 def parse_statement(file_name):
-    file_stem = Path(file_name).stem
-    pages = get_elements_from_pdf(file_name)
-    first_page_elements = pages[0].elements
-    first_page_last_line = first_page_elements[len(first_page_elements) - 1].get_text()
-    if first_page_last_line.startswith('United Overseas Bank Limited'):
-        parse_uob_statement(file_stem, pages, first_page_last_line.split(' • '))
+    file = Path(file_name)
+    file_extension = file.suffix
+    file_stem = file.stem
+    match file_extension.casefold():
+        case '.pdf':
+            pages = get_elements_from_pdf(file_name)
+            first_page_elements = pages[0].elements
+            first_page_last_line = first_page_elements[len(first_page_elements) - 1].get_text()
+            if first_page_last_line.startswith('United Overseas Bank Limited'):
+                parse_uob_statement(file_stem, pages, first_page_last_line.split(' • '))
+        case '.csv':
+            with open(file_name, 'r') as csvfile:
+                csvreader = csv.reader(csvfile)
+                collected_rows = [row for row in csvreader if row]
+
+            if collected_rows[0][1].startswith('POSB'):
+                parse_posb_account_transactions(file_stem, InstrumentHolder.objects.get(pk=1), 'SGD', collected_rows)
 
 
-parse_statement('C:\\Users\\AmideWing\\Downloads\\eStatement_17272.506835308788.pdf')
+parse_statement('C:\\Users\\AmideWing\\Downloads\\c177d2d42da4b21c8b2d02192aa3a08b.P000000021437251.csv')
